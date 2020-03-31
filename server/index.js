edab61ce21b8fe998a7be24d0cfc29c741fee8b7
Phillip Rousu <philliprousu@gmail.com>
2020-03-31 16:39:08 -0700

    update nodemon

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const port = 3001;
 const App = require('../db/App.js');
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.json());
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
@@ -17,10 +18,25 @@ app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/api/apps', (req, res) => {
   App.find({}, (err, apps) => {
     if (err) {
-      console.log('hey there was an error');
+      console.log('hey  there was an error');
     }
     res.send(apps);
   }).limit(5);
 });
+
+app.post('/api/apps', (req, res) => {
+  let newApp = new App({
+    _id: req.body._id,
+    name: req.body.name,
+    logo: req.body.logo,
+    company: req.body.company,
+    rating: req.body.rating,
+    description: req.body.description
+  });
+  App.create(newApp)
+    .then(result => res.json(result))
+    .catch(e => res.json(e));
+});
+
 
 app.listen(port, () => console.log(`Similar Component listening on port ${port}!`));
