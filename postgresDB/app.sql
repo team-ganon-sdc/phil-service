cfb70d49d5cecd06062e105e92376babe409f78b
Phillip Rousu <philliprousu@gmail.com>
2020-04-02 20:19:28 -0700

    adjusted schema

--- postgresDB/app.sql
+++ postgresDB/app.sql
@@ -2,9 +2,9 @@
 CREATE TABLE allApps (
   appid integer UNIQUE PRIMARY KEY,
   relatedappid integer ARRAY[5],
-  name varchar(50),
-  logo varchar(100),
-  company varchar(50),
+  name varchar(200),
+  logo varchar(200),
+  company varchar(200),
   rating integer,
   description varchar(250)
 );
