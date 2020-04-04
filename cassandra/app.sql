8dd71c5afade13336f0195182cc799fb7e5259a7
Phillip Rousu <philliprousu@gmail.com>
2020-04-03 20:33:56 -0700

    explored schemas

--- cassandra/app.sql
+++ cassandra/app.sql
@@ -1,4 +1,8 @@
 
+-- create keyspace allApps with replication = {'class':'SimpleStrategy', 'replication_factor': 1};
+
+-- use allapps;
+
 CREATE TABLE allApps (
   appid int PRIMARY KEY,
   relatedappid list<int>,
@@ -9,8 +13,13 @@ CREATE TABLE allApps (
   description text
 );
 
-
-uuid?
+--cassandra does not do join queries so dont bother
+-- CREATE TABLE similar (
+--   id int,
+--   appid int,
+--   relatedapp int,
+--   PRIMARY KEY (id, appid, relatedapp)
+-- )
 
 const appSchema = new Schema({
   _id: Number,
