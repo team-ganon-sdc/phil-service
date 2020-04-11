c4c9f2e49d799ba2f1fa0a9a38a7894551eead7a
Phillip Rousu <philliprousu@gmail.com>
2020-04-11 15:49:44 -0700

    loaderio path

--- server/index.js
+++ server/index.js
@@ -33,6 +33,10 @@ pool.connect()
   .catch(e => console.log(e));
 
 
+app.get('/loaderio-939f6b204a8162a1afadfc0dc4474eae.txt', (req, res) => {
+  res.sendFile(path.join(__dirname, '../loaderio-939f6b204a8162a1afadfc0dc4474eae.txt'));
+});
+
 app.get('/api/apps/:id', (req, res) => {
   // eslint-disable-next-line quotes
   let query = `select appid, name, logo, company, rating, description from allapps WHERE appid = (select relatedappid[1] from allapps where appid = ${req.params.id}) or appid = (select relatedappid[2] from allapps where appid = ${req.params.id}) or appid = (select relatedappid[3] from allapps where appid = ${req.params.id}) or appid = (select relatedappid[4] from allapps where appid = ${req.params.id}) or appid = (select relatedappid[5] from allapps where appid = ${req.params.id})`;
