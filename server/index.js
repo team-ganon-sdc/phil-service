5b80d3777917dfa02f612991ad5373c6884ab2fb
Phillip Rousu <philliprousu@gmail.com>
2020-04-11 13:56:48 -0700

    cors workaround

--- server/index.js
+++ server/index.js
@@ -5,16 +5,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { Pool } = require('pg');
+var cors = require('cors')
 
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
+app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
 
 
